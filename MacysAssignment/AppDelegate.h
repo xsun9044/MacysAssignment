ad4c8bf0b427e9cf9154a62f620c439ef387bb78
Xin Sun <Xin@xindembp.home>
2015-10-06 18:36:37 -0400

    Initial Commit

---
+++ MacysAssignment/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MacysAssignment
+//
+//  Created by Xin Sun on 10/6/15.
+//  Copyright © 2015 Xin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MacysAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MacysAssignment
+//
+//  Created by Xin Sun on 10/6/15.
+//  Copyright © 2015 Xin Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
